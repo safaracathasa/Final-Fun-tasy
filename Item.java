5e62f0da7cbf3359e7d74b8dd90ba1e9b1d9d9cd
huntul <huntul.dx@gmail.com>
2015-04-12 17:51:31 +0700

    Create Item.java

---
+++ Item.java
@@ -0,0 +1,30 @@
+package dev.huntul.finalfuntasy.item;
+
+public class Item {
+	private int value;
+	private int cost;
+	private String desc;
+	public Item(int value, int cost, String desc) {
+		this.value = value;
+		this.cost = cost;
+		this.desc = desc;
+	}
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
+	public int getCost() {
+		return cost;
+	}
+	public void setCost(int cost) {
+		this.cost = cost;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+}
