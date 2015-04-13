8d1db2ae7125de9b0e6b2ed3ad5727fbb914f8c1
Nurizka Fitrianingrum <nurizkaf96@gmail.com>
2015-04-13 08:45:06 +0700

    Create Battle.java

---
+++ Battle.java
@@ -0,0 +1,15 @@
+// Kelas Battle, berisi berbagai mekanisme saat pemain memasuki battle scene
+// Last edit: 10/04-2015
+
+package BagianDzaka_01;
+
+public class Battle {
+    // Finish to Start: supaya bisa dikerjakan, kelas ini butuh kelas karakter, monster, dan item
+    // Kesimpulan: belum dapat dikerjakan ;)
+
+    // Framework kelas: command saat battle
+    // public void attack()
+    // public void magic()
+    // public void item()
+    // public void defend()
+}
