e45f5c399ce5cef1cf4e41420d1737b446f0524a
huntul <huntul.dx@gmail.com>
2015-04-17 15:09:41 +0700

    Update PosInfo.java

--- PosInfo.java
+++ PosInfo.java
@@ -1,51 +1,41 @@
-// Kelas PosInfo, berisi informasi posisi fix dimana toko, item, dan monster berada pada map
-// Last edit: 10/04-2015
-
-package BagianDzaka_01;
+package dev.huntul.finalfuntasy.etc;
 
 public class PosInfo {
-    private int posX; // Skala 0~9
-    private int posY; // Skala 0~9
-    
-    public void setX(int x){
-        posX = x;
-    }
-    public void setY(int y){
-        posY = y;
-    }
-    
-    public boolean cekToko(){
+	public PosInfo() {
+		
+	}
+	public boolean cekToko(int x, int y) {
         // Mengembalikan True jika posisi pemain tepat berada pada posisi toko
         // Silakan diedit bagian "KONSTANTA", menyesuaikan dengan kebutuhan
 
         // KONSTANTA
         // posisi toko 1
-        int x1 = 1;
-        int y1 = 2;
+        int x1 = 0;
+        int y1 = 9;
         // posisi toko 2
-        int x2 = 2;
-        int y2 = 3;
+        int x2 = 7;
+        int y2 = 8;
         // posisi toko 3
-        int x3 = 3;
+        int x3 = 0;
         int y3 = 4;
         // posisi toko 4
-        int x4 = 4;
-        int y4 = 5;
+        int x4 = 9;
+        int y4 = 1;
         // posisi toko 5
         int x5 = 5;
-        int y5 = 6;
+        int y5 = 5;
         
         // PENGECEKAN
-        boolean test1 = (posX==x1 && posY==y1);
-        boolean test2 = (posX==x2 && posY==y2);
-        boolean test3 = (posX==x3 && posY==y3);
-        boolean test4 = (posX==x4 && posY==y4);
-        boolean test5 = (posX==x5 && posY==y5);
+        boolean test1 = (x==x1 && y==y1);
+        boolean test2 = (x==x2 && y==y2);
+        boolean test3 = (x==x3 && y==y3);
+        boolean test4 = (x==x4 && y==y4);
+        boolean test5 = (x==x5 && y==y5);
         
         return(test1||test2||test3||test4||test5);
     }
     
-    public boolean cekItem(){
+    public boolean cekItem(int x, int y) {
         // Mengembalikan True jika posisi pemain tepat berada pada posisi item
         // Silakan diedit bagian "KONSTANTA", menyesuaikan dengan kebutuhan
 
@@ -54,49 +44,49 @@ public class PosInfo {
         int x01 = 2;
         int y01 = 1;
         // posisi item 02
-        int x02 = 3;
-        int y02 = 2;
+        int x02 = 9;
+        int y02 = 9;
         // posisi item 03
-        int x03 = 4;
-        int y03 = 3;
+        int x03 = 5;
+        int y03 = 2;
         // posisi item 04
-        int x04 = 5;
-        int y04 = 4;
+        int x04 = 0;
+        int y04 = 7;
         // posisi item 05
-        int x05 = 6;
-        int y05 = 5;
+        int x05 = 3;
+        int y05 = 0;
         // posisi item 06
         int x06 = 7;
         int y06 = 6;
         // posisi item 07
-        int x07 = 8;
-        int y07 = 7;
+        int x07 = 2;
+        int y07 = 9;
         // posisi item 08
         int x08 = 9;
-        int y08 = 8;
+        int y08 = 7;
         // posisi item 09
-        int x09 = 0;
-        int y09 = 9;
+        int x09 = 1;
+        int y09 = 5;
         // posisi item 10
-        int x10 = 9;
-        int y10 = 0;
+        int x10 = 4;
+        int y10 = 4;
         
         // PENGECEKAN
-        boolean test01 = (posX==x01 && posY==y01);
-        boolean test02 = (posX==x02 && posY==y02);
-        boolean test03 = (posX==x03 && posY==y03);
-        boolean test04 = (posX==x04 && posY==y04);
-        boolean test05 = (posX==x05 && posY==y05);
-        boolean test06 = (posX==x06 && posY==y06);
-        boolean test07 = (posX==x07 && posY==y07);
-        boolean test08 = (posX==x08 && posY==y08);
-        boolean test09 = (posX==x09 && posY==y09);
-        boolean test10 = (posX==x10 && posY==y10);
+        boolean test01 = (x==x01 && y==y01);
+        boolean test02 = (x==x02 && y==y02);
+        boolean test03 = (x==x03 && y==y03);
+        boolean test04 = (x==x04 && y==y04);
+        boolean test05 = (x==x05 && y==y05);
+        boolean test06 = (x==x06 && y==y06);
+        boolean test07 = (x==x07 && y==y07);
+        boolean test08 = (x==x08 && y==y08);
+        boolean test09 = (x==x09 && y==y09);
+        boolean test10 = (x==x10 && y==y10);
         
         return(test01||test02||test03||test04||test05||test06||test07||test08||test09||test10);
     }
     
-    public boolean cekMonster(){
+    public boolean cekMonster(int x, int y) {
         // Mengembalikan True jika posisi pemain tepat berada pada posisi monster
         // Silakan diedit bagian "KONSTANTA", menyesuaikan dengan kebutuhan
 
@@ -163,26 +153,26 @@ public class PosInfo {
         int y20 = 3;
         
         // PENGECEKAN
-        boolean test01 = (posX==x01 && posY==y01);
-        boolean test02 = (posX==x02 && posY==y02);
-        boolean test03 = (posX==x03 && posY==y03);
-        boolean test04 = (posX==x04 && posY==y04);
-        boolean test05 = (posX==x05 && posY==y05);
-        boolean test06 = (posX==x06 && posY==y06);
-        boolean test07 = (posX==x07 && posY==y07);
-        boolean test08 = (posX==x08 && posY==y08);
-        boolean test09 = (posX==x09 && posY==y09);
-        boolean test10 = (posX==x10 && posY==y10);
-        boolean test11 = (posX==x11 && posY==y11);
-        boolean test12 = (posX==x12 && posY==y12);
-        boolean test13 = (posX==x13 && posY==y13);
-        boolean test14 = (posX==x14 && posY==y14);
-        boolean test15 = (posX==x15 && posY==y15);
-        boolean test16 = (posX==x16 && posY==y16);
-        boolean test17 = (posX==x17 && posY==y17);
-        boolean test18 = (posX==x18 && posY==y18);
-        boolean test19 = (posX==x19 && posY==y19);
-        boolean test20 = (posX==x20 && posY==y20);
+        boolean test01 = (x==x01 && y==y01);
+        boolean test02 = (x==x02 && y==y02);
+        boolean test03 = (x==x03 && y==y03);
+        boolean test04 = (x==x04 && y==y04);
+        boolean test05 = (x==x05 && y==y05);
+        boolean test06 = (x==x06 && y==y06);
+        boolean test07 = (x==x07 && y==y07);
+        boolean test08 = (x==x08 && y==y08);
+        boolean test09 = (x==x09 && y==y09);
+        boolean test10 = (x==x10 && y==y10);
+        boolean test11 = (x==x11 && y==y11);
+        boolean test12 = (x==x12 && y==y12);
+        boolean test13 = (x==x13 && y==y13);
+        boolean test14 = (x==x14 && y==y14);
+        boolean test15 = (x==x15 && y==y15);
+        boolean test16 = (x==x16 && y==y16);
+        boolean test17 = (x==x17 && y==y17);
+        boolean test18 = (x==x18 && y==y18);
+        boolean test19 = (x==x19 && y==y19);
+        boolean test20 = (x==x20 && y==y20);
         
         return(test01||test02||test03||test04||test05||test06||test07||test08||test09||test10||test11||test12||test13||test14||test15||test16||test17||test18||test19||test20);
     }
