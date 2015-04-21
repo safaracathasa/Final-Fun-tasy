bc0681dc4444cc50542fdd664c5cba8e97c3bff5
Safara Cathasa Riverinda Rijadi <safara.rijadi@gmail.com>
2015-04-21 21:29:35 +0700

    Create Kingsman.java

---
+++ Kingsman.java
@@ -0,0 +1,11 @@
+public class Kingsman extends Monster {
+
+    public Kingsman ()
+    {
+        this.ATK = 5;
+        this.maxHP = 200;
+        this.CurHP = 200;
+        this.money = 50;
+        this.item = {"Potion","Ether","Bomb"};
+    }
+}
