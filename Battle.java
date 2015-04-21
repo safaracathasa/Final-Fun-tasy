fc4d9bed82ac7ba6347896a44ffe04fb99ec1048
Zonkies <mychar_id@yahoo.co.id>
2015-04-21 22:35:21 +0700

    Create Battle.java

---
+++ Battle.java
@@ -0,0 +1,54 @@
+// Kelas Battle, v1.00
+
+import java.util.Random;
+import java.util.Scanner;
+public class Battle{ 
+    Random rand = new Random();
+    String[] monsterName = {"Kingsman","Behemoth","Marlboro"};
+    int statusBattle = 0; // 0 masih battle, 1 win, 2 lose
+    int i = rand.nextInt(3);
+    Monster M = new ?(monsterName[i]);
+    
+    public void printStatus(Pemain P){
+        for(int i=0;i<=2;i++){
+            System.out.println("HP "+P.getCharacters()[i].getName()+": "+P.getCharacters()[i].getCurHP());
+            System.out.println("MP "+P.getCharacters()[i].getName()+": "+P.getCharacters()[i].getCurMP()+"\n");
+        }
+        System.out.println("HP "+M.getClass()+": "+M.getCurHP()+"\n");
+    }
+    
+    public void runBattle(Pemain P){
+        Scanner in = new Scanner(System.in);
+        int command;
+        do{
+            for(int j=0;j<=2;j++){
+                    System.out.println("Giliran " + P.getCharacters()[j].getName());
+                    printStatus(P);
+                    System.out.println("(1)Attack, (2)Magic, (3)Item");
+                do{
+                    command = in.nextInt();
+                    switch (command) {
+                                    case 1: {
+                                        P.getCharacters()[j].useAttack(M);
+                                        System.out.println(P.getCharacters()[j].getName() + " menyerang " + M.getClass());
+                                        
+                                    }
+                                    case 2: {
+
+                                    }
+                                    case 3: {
+
+                                    }
+                                    default: {
+                                        System.out.println("Masukan salah! ulangi");
+                                    }
+                    }
+                } while((command<1)||(command>3));
+            }
+            System.out.println("Giliran " + M.getClass());
+            printStatus(P);
+            M.useAttack(P.getCharacters()[rand.nextInt(3)]);
+        }while(statusBattle == 0);
+        
+    }
+}
