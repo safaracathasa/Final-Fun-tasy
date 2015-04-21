3a5763d4b8ad0a77dcc6954951e5e969a4349b94
Safara Cathasa Riverinda Rijadi <safara.rijadi@gmail.com>
2015-04-21 21:30:40 +0700

    Create Behemoth.java

---
+++ Behemoth.java
@@ -0,0 +1,10 @@
+public class Behemoth extends Monster {
+    public Behemoth()
+    {
+        this.ATK = 10;
+        this.maxHP = 250;
+        this.CurHP = 250;
+        this.money = 80;
+        this.item = {"Potion","Ether","Bomb"};
+    }
+}
