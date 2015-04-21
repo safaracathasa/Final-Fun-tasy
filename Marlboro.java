f47c8ea0941523d7c9bfbb228fdcd51b9ea6aba9
Safara Cathasa Riverinda Rijadi <safara.rijadi@gmail.com>
2015-04-21 21:30:07 +0700

    Create Marlboro.java

---
+++ Marlboro.java
@@ -0,0 +1,10 @@
+public class Marlboro extends Monster {
+    public Marlboro ()
+    {
+        this.ATK = 7;
+        this.maxHP = 250;
+        this.CurHP = 250;
+        this.money = 70;
+        this.item = {"Potion","Ether","Bomb"};
+    }  
+}
