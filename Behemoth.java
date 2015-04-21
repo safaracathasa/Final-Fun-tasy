fb4dc31b3481003a58ebff538ae55cde9407962f
Safara Cathasa Riverinda Rijadi <safara.rijadi@gmail.com>
2015-04-21 22:37:13 +0700

    Update Behemoth.java

--- Behemoth.java
+++ Behemoth.java
@@ -1,11 +1,12 @@
 public class Behemoth extends Monster {
-    public Behemoth(Characters chars)
+    
+    public Behemoth(Character chars)
     {
         super(chars);
-        this.ATK = 10;
-        this.maxHP = 250;
-        this.CurHP = 250;
-        this.money = 80;
+        this.ATK = 10*x;
+        this.maxHP = 250*x;
+        this.CurHP = 250*x;
+        this.money = 80*x;
         this.item = {"Potion","Ether","Bomb"};
     }
 }
