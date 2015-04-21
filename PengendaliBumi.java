0aca93596d781dbb88be6efd49b6a499f96e3b20
Nurizka Fitrianingrum <nurizkaf96@gmail.com>
2015-04-21 22:00:59 +0700

    Create PengendaliBumi.java

---
+++ PengendaliBumi.java
@@ -0,0 +1,33 @@
+public class PengendaliBumi extends Character
+{
+	public Hero(String nama)
+	{
+		super(nama, 12, 10, 8);
+	}
+
+	public void useMagic(Monster mon)
+	{
+
+		if ((this.cLV <= 10)
+		{
+			System.out.println("Terre!");
+			mon.setHP(mon.getHP - this.damageMagic);
+		}
+		else if ((this.cLV >= 11) && (this.cLV <=20))
+		{
+			System.out.println("Terra!");
+			mon.setHP(mon.getHP - this.damageMagic - 20);
+		}
+		else if ((this.cLV >= 21) && (this.cLV <=30))
+		{
+			System.out.println("Terraga!");
+			mon.setHP(mon.getHP - this.damageMagic - 50);
+		}
+		else 
+		{
+			System.out.println("Armageddon!");
+			mon.setHP(mon.getHP - this.damageMagic - 100);
+		}		
+
+	}
+}
