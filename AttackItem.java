9c53e2458d1fe1e207a42ee43ae274ddc038e4ad
huntul <huntul.dx@gmail.com>
2015-04-23 12:19:00 +0700

    Update AttackItem.java

--- AttackItem.java
+++ AttackItem.java
@@ -1,7 +1,14 @@
 package dev.huntul.finalfuntasy.item;
 
+import dev.huntul.finalfuntasy.character.CharacterGame;
+
+
 public class AttackItem extends Item {
-	public AttackItem(int value, int cost, String desc) {
-		super(value,cost,desc);
+	public AttackItem(String name, int value, int cost) {
+		super(name,value,cost);
+	}
+	
+	public <U> void effectItem(U u) {
+		((CharacterGame) u).setCurHP(((CharacterGame) u).getCurHP() + getValue());
 	}
 }
