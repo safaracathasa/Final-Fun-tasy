9227d6f287ee240860297b0575d0e47915871da1
Zonkies <mychar_id@yahoo.co.id>
2015-04-23 12:42:50 +0700

    Update Marlboro.java

--- Marlboro.java
+++ Marlboro.java
@@ -1,11 +1,11 @@
 public class Marlboro extends Monster {
 
-    public Marlboro (CharacterGame chars)
+    public Marlboro(CharacterGame chars1,CharacterGame chars2,CharacterGame chars3)
     {
-        super(chars);
-        this.ATK = this.ATK*15;
-        this.maxHP = this.maxHP*400;
-        this.CurHP = this.CurHP*400;
-        this.money = this.money*120;
+        super(chars1,chars2,chars3);
+        this.ATK = (this.ATK*8)+15;
+        this.maxHP = (this.maxHP*70)+400;
+        this.CurHP = (this.CurHP*70)+400;
+        this.money = (this.money*10)+120;
     }
 }
