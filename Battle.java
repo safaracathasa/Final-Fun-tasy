5d9127971624d92db42cbee4b1beb360b1493fe4
Zonkies <mychar_id@yahoo.co.id>
2015-04-23 13:45:22 +0700

    Update Battle.java

--- Battle.java
+++ Battle.java
@@ -1,60 +1,94 @@
-// Kelas Battle, V1.01
-
 import java.util.Random;
 import java.util.Scanner;
 public class Battle{ 
     Random rand = new Random();
-    String[] monsterName = {"Kingsman","Behemoth","Marlboro"};
     int statusBattle = 0; // 0 masih battle, 1 win, 2 lose
-    int i = rand.nextInt(3);
-    Monster M = new ?(monsterName[i]);
+    int[] battleOrder = {0,1,2};
     
-    public void printStatus(Pemain P){
+    public void printStatus(Pemain P, Monster M){
         for(int i=0;i<=2;i++){
-            System.out.println("HP "+P.getCharacters()[i].getName()+": "+P.getCharacters()[i].getCurHP());
-            System.out.println("MP "+P.getCharacters()[i].getName()+": "+P.getCharacters()[i].getCurMP()+"\n");
+            System.out.println("HP "+P.getCharacters()[i].getNama()+": "+P.getCharacters()[i].getCurHP());
+            System.out.println("MP "+P.getCharacters()[i].getNama()+": "+P.getCharacters()[i].getCurMP()+"\n");
         }
         System.out.println("HP "+M.getClass()+": "+M.getCurHP()+"\n");
     }
+    public void attack(CharacterGame C, Monster M){
+        C.useAttack(M);
+        System.out.println(C.getNama() + " menyerang " + M.getClass());
+    }
+    public void magic(CharacterGame C, Monster M){
+        //C.useMagic(M);
+        System.out.println(C.getNama() + " menggunakan skill kepada " + M.getClass());
+    }
+    public void item(CharacterGame C, Monster M){
+        
+    }
+    public void monsterAttack(CharacterGame C, Monster M){
+        M.useAttack(C);
+        System.out.println(M.getClass()+ " Menyerang " + C.getNama());
+    }
+    public boolean isCharacterDied(CharacterGame C){
+        if(C.getCurHP() <= 0){
+            C.setCurHP(0);
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
     
-    public void runBattle(Pemain P){
+    public boolean isMonsterDied(Monster M){
+        return M.getCurHP() <= 0;
+    }
+    
+    public int runBattle(Pemain P, Monster M){
         Scanner in = new Scanner(System.in);
-        int command;
+        boolean isNextTurn = false;
+        int command,p;
         do{
             for(int j=0;j<=2;j++){
-                    System.out.println("Giliran " + P.getCharacters()[j].getName());
-                    printStatus(P);
-                    System.out.println("(1)Attack, (2)Magic, (3)Item");
-                do{
-                    command = in.nextInt();
-                    switch (command) {
-                                    case 1: {
-                                        P.getCharacters()[j].useAttack(M);
-                                        System.out.println(P.getCharacters()[j].getName() + " menyerang " + M.getClass());
-                                        
-                                    }
-                                    case 2: { 
-                                        System.out.println("Pilih Magic");
-                                        for(int i=0;i<"Magic".length;i++){
-                                            System.out.println("(" + i + ") " + "Magic"[i].name);
-                                        }
-                                        int k = in.nextInt();
-                                        //try catch untuk MPnya, implementasi ya tul wkwk
-                                        P.getCharacters()[k].useMagic(M);
-                                    }
-                                    case 3: { // kelas itemnya dibuat duluu
-                                            
-                                    }
-                                    default: {
-                                        System.out.println("Masukan salah! ulangi");
-                                    }
+                if(!isCharacterDied(P.getCharacters()[j]))
+                    isNextTurn = false;
+                    do{
+                        System.out.println("Giliran " + P.getCharacters()[j].getNama());
+                        printStatus(P,M);
+                        System.out.println("(1)Attack, (2)Magic, (3)Item");
+                        do{
+                            command = in.nextInt();
+                            switch (command) {
+                                            case 1: {
+                                                attack(P.getCharacters()[j],M);
+                                                isNextTurn = true;
+                                            }
+                                            case 2: {
+                                                magic(P.getCharacters()[j],M);
+                                                isNextTurn = true;
+                                            }
+                                            case 3: {
+                                                isNextTurn = true;
+                                            }
+                                            default: {
+                                                System.out.println("Masukan salah! ulangi");
+                                            }
+                            }
+                        } while((command<1)||(command>3));
+                    }while(!isNextTurn);
+                }
+                if(isMonsterDied(M)){
+                    statusBattle = 1;
+                }
+                else{
+                    System.out.println("Giliran " + M.getClass());
+                    printStatus(P,M);
+                    do{
+                        p = rand.nextInt(3);
+                    } while(isCharacterDied(P.getCharacters()[p]));
+                    monsterAttack(P.getCharacters()[p],M);
+                    if(isCharacterDied(P.getCharacters()[0]) && isCharacterDied(P.getCharacters()[1]) && isCharacterDied(P.getCharacters()[2])){
+                        statusBattle = 2;
                     }
-                } while((command<1)||(command>3));
-            }
-            System.out.println("Giliran " + M.getClass());
-            printStatus(P);
-            M.useAttack(P.getCharacters()[rand.nextInt(3)]);
+                }
         }while(statusBattle == 0);
-        
+        return statusBattle;
     }
 }
