11bd1cfe44b646f09d9b47c531a0b0af0b56df5b
huntul <huntul.dx@gmail.com>
2015-04-23 12:00:14 +0700

    Create ItemFactory.java

---
+++ ItemFactory.java
@@ -0,0 +1,19 @@
+package dev.huntul.finalfuntasy.item;
+
+import java.util.Random;
+
+public class ItemFactory {
+	public static Item createItem() {
+		Item item = null;
+		Random rand = new Random();
+		int token = rand.nextInt(5);
+		if ((token == 0) || (token == 1) || (token == 2)) {
+			item = new HealingItem("Potion",50,50);
+		} else if ((token == 3) || (token == 4)) {
+			item = new HealingItem("Ether",25,100);
+		} else {
+			item = new AttackItem("Bomb",100,200);
+		}
+		return item;
+	}
+}
