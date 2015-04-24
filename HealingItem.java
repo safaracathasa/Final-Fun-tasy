618864f61ab733498066db08d3a639cd41d8525a
huntul <huntul.dx@gmail.com>
2015-04-24 20:58:45 +0700

    Update HealingItem.java

--- HealingItem.java
+++ HealingItem.java
@@ -1,14 +1,35 @@
 package dev.huntul.finalfuntasy.item;
 
 import dev.huntul.finalfuntasy.character.CharacterGame;
+import dev.huntul.finalfuntasy.monster.Monster;
 
 
 public class HealingItem extends Item {
-	public HealingItem(String name, int value, int cost) {
+	private int id;
+	public HealingItem(String name, int value, int cost, int id) {
 		super(name,value,cost);
+		this.id = id;
 	}
 
-	public <U> void effectItem(U u) {
-		((CharacterGame) u).setCurHP(((CharacterGame) u).getCurHP() + getValue());
+	public void effectItem(CharacterGame chars, Monster monster) {
+		if (id == 1) {
+			chars.setCurHP(chars.getCurHP() + getValue());
+			if (chars.getCurHP() >= chars.getMaxHP()) {
+				chars.setCurHP(chars.getMaxHP());
+			}
+		} else {
+			chars.setCurMP(chars.getCurMP() + getValue());
+			if (chars.getCurMP() >= chars.getMaxMP()) {
+				chars.setCurMP(chars.getMaxMP());
+			}
+		}
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
 	}
 }
