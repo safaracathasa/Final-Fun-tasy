5b5de6e176b4cf9e098056c8211f398c6761cde1
huntul <huntul.dx@gmail.com>
2015-04-24 20:59:44 +0700

    Update ItemFactory.java

--- ItemFactory.java
+++ ItemFactory.java
@@ -8,9 +8,9 @@ public class ItemFactory {
 		Random rand = new Random();
 		int token = rand.nextInt(5);
 		if ((token == 0) || (token == 1) || (token == 2)) {
-			item = new HealingItem("Potion",50,50);
+			item = new HealingItem("Potion",50,50,1);
 		} else if ((token == 3) || (token == 4)) {
-			item = new HealingItem("Ether",25,100);
+			item = new HealingItem("Ether",25,100,2);
 		} else {
 			item = new AttackItem("Bomb",100,200);
 		}
