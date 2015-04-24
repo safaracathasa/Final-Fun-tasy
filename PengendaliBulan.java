bec6312690405022531ee018f14b1e04b21e1772
huntul <huntul.dx@gmail.com>
2015-04-24 21:02:42 +0700

    Update PengendaliBulan.java

--- PengendaliBulan.java
+++ PengendaliBulan.java
@@ -21,23 +21,26 @@ public class PengendaliBulan extends CharacterGame
 		if (this.cLV <= 10)
 		{
 			System.out.println("Moon!");
+			curMP = curMP - 20;
 			mon.setCurHP(mon.getCurHP() - damageMagic());
 		}
 		else if ((this.cLV >= 11) && (this.cLV <=20))
 		{
 			System.out.println("Moona!");
+			curMP = curMP - 30;
 			mon.setCurHP(mon.getCurHP() - damageMagic() - 20);
 		}
 		else if ((this.cLV >= 21) && (this.cLV <=30))
 		{
 			System.out.println("Moonaga!");
+			curMP = curMP - 40;
 			mon.setCurHP(mon.getCurHP() - damageMagic() - 50);
 		}
 		else 
 		{
 			System.out.println("Gerhana Bulan!");
+			curMP = curMP - 50;
 			mon.setCurHP(mon.getCurHP() - damageMagic() - 100);
 		}		
-
 	}
 }
