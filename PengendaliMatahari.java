d9d9b64a05e9e87129e1e10ef98ab86ca6c2f919
huntul <huntul.dx@gmail.com>
2015-04-24 21:04:20 +0700

    Update PengendaliMatahari.java

--- PengendaliMatahari.java
+++ PengendaliMatahari.java
@@ -21,23 +21,26 @@ public class PengendaliMatahari extends CharacterGame
 		if (this.cLV <= 10)
 		{
 			System.out.println("Sun!");
+			curMP = curMP - 20;
 			mon.setCurHP(mon.getCurHP() - damageMagic());
 		}
 		else if ((this.cLV >= 11) && (this.cLV <=20))
 		{
 			System.out.println("Suna!");
+			curMP = curMP - 30;
 			mon.setCurHP(mon.getCurHP() - damageMagic() - 20);
 		}
 		else if ((this.cLV >= 21) && (this.cLV <=30))
 		{
 			System.out.println("Sunaga!");
+			curMP = curMP - 40;
 			mon.setCurHP(mon.getCurHP() - damageMagic() - 50);
 		}
 		else 
 		{
 			System.out.println("Gerhana Matahari!");
+			curMP = curMP - 50;
 			mon.setCurHP(mon.getCurHP() - damageMagic() - 100);
 		}		
-
 	}
 }
