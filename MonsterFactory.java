cb19aa17cca2310a258401722aae316e917a2de4
huntul <huntul.dx@gmail.com>
2015-04-25 22:29:34 +0700

    Update MonsterFactory.java

--- MonsterFactory.java
+++ MonsterFactory.java
@@ -8,9 +8,9 @@ public class MonsterFactory {
 	public static Monster createMonster(CharacterGame chars1,CharacterGame chars2,CharacterGame chars3) {
 		Monster monster = null;
 		Random rand = new Random();
-		int token = rand.nextInt(5);
+		int token = rand.nextInt(6);
 		if ((token == 0) || (token == 1) || (token == 2)) {
-			monster = new Kingsman(chars1,chars2,chars3);
+			monster = new Goblin(chars1,chars2,chars3);
 		} else if ((token == 3) || (token == 4)) {
 			monster = new Behemoth(chars1,chars2,chars3);
 		} else {
