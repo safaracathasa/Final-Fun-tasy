17ede4a76cec6a5fdbee76fd78f7e921937e06ca
huntul <huntul.dx@gmail.com>
2015-04-25 22:24:45 +0700

    Update AttackItem.java

--- AttackItem.java
+++ AttackItem.java
@@ -7,9 +7,11 @@ import dev.huntul.finalfuntasy.monster.Monster;
 public class AttackItem extends Item {
 	public AttackItem(String name, int value, int cost) {
 		super(name,value,cost);
+		setType(2);
 	}
 	
 	public void effectItem(CharacterGame chars, Monster monster) {
 		monster.setCurHP(monster.getCurHP() - getValue());
+		System.out.println("Hit " + getValue() + " damage!");
 	}
 }
