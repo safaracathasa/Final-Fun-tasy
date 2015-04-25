090bc92faf25082eab4706cf190ff7cbd030b77a
huntul <huntul.dx@gmail.com>
2015-04-25 22:26:43 +0700

    Update HealingItem.java

--- HealingItem.java
+++ HealingItem.java
@@ -9,16 +9,21 @@ public class HealingItem extends Item {
 	public HealingItem(String name, int value, int cost, int id) {
 		super(name,value,cost);
 		this.id = id;
+		setType(1);
 	}
 
 	public void effectItem(CharacterGame chars, Monster monster) {
 		if (id == 1) {
 			chars.setCurHP(chars.getCurHP() + getValue());
+			System.out.println("Use " + getName() + " on " + chars.getNama());
+			System.out.println("Healed " + getValue() + " HP");
 			if (chars.getCurHP() >= chars.getMaxHP()) {
 				chars.setCurHP(chars.getMaxHP());
 			}
 		} else {
 			chars.setCurMP(chars.getCurMP() + getValue());
+			System.out.println("Use " + getName() + " on " + chars.getNama());
+			System.out.println("Healed " + getValue() + " MP");
 			if (chars.getCurMP() >= chars.getMaxMP()) {
 				chars.setCurMP(chars.getMaxMP());
 			}
