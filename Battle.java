292863e2248e1e08220d3fa9ef3ef7934f04193a
huntul <huntul.dx@gmail.com>
2015-04-25 22:25:10 +0700

    Update Battle.java

--- Battle.java
+++ Battle.java
@@ -22,6 +22,7 @@ public class Battle{
         System.out.println();
         System.out.println(C.getNama() + " menyerang " + M.getName());
         C.useAttack(M);
+        System.out.println();
     }
     public void magic(CharacterGame C, Monster M){
         System.out.println();
@@ -41,23 +42,46 @@ public class Battle{
 				System.out.println(i + ". " + P.getStocks()[i-1].getItem().getName() + " : " + P.getStocks()[i-1].getStock());
 				i++;
 			}
-			int command;
+			int command, cmd;
+	        Scanner in = new Scanner(System.in);
 			do {
-		        Scanner in = new Scanner(System.in);
 				System.out.print("\nCommand : ");
 				command = in.nextInt();
 				if (command <1 || command > i-1) {
 					System.out.println("Command not found!");
 				}
 			} while (command < 1 || command > i-1);
-			P.getCharacters()[j].useItem(P.getStocks()[command-1].getItem(), P.getCharacters()[j], M);
+			if (P.getStocks()[command-1].getItem().getType() == 1) {
+				System.out.println("Choose target :");
+				for (int k = 1; k <= 3; k++) {
+					System.out.println(k + ". " + P.getCharacters()[k-1].getNama());
+				}
+				do {
+					System.out.print("\nCommand : ");
+					cmd = in.nextInt();
+					if (cmd <1 || cmd > 3) {
+						System.out.println("Command not found!");
+					}
+				} while (cmd < 1 || cmd > 3);
+				switch (cmd) {
+					case 1	: P.getCharacters()[j].useItem(P.getStocks()[command-1].getItem(), P.getCharacters()[0], M);
+							  break;
+					case 2	: P.getCharacters()[j].useItem(P.getStocks()[command-1].getItem(), P.getCharacters()[1], M);
+					  		  break;
+					case 3	: P.getCharacters()[j].useItem(P.getStocks()[command-1].getItem(), P.getCharacters()[2], M);
+							  break;
+				}
+			} else {
+				P.getCharacters()[j].useItem(P.getStocks()[command-1].getItem(), P.getCharacters()[j], M);
+			}
 			P.delItem(P.getStocks()[command-1].getItem());
 		}
         System.out.println();
     }
     public void monsterAttack(CharacterGame C, Monster M){
-        M.useAttack(C);
         System.out.println(M.getName()+ " Menyerang " + C.getNama());
+        M.useAttack(C);
+        System.out.println();
     }
     public boolean isCharacterDied(CharacterGame C){
         return C.getCurHP() <= 0;
@@ -75,8 +99,10 @@ public class Battle{
         System.out.println();
         System.out.println(M.getName() + " draws near!");
         System.out.println();
+        M.printMonster();
         do{
-            for(int j=0;j<=2;j++){
+        	int j = 0;
+            while((j <= 2) && !isMonsterDied(M)) {
                 if(!isCharacterDied(P.getCharacters()[j])){
                     isNextTurn = false;
                     do{
@@ -108,12 +134,14 @@ public class Battle{
                         } while((command<1)||(command>3));
                     }while(!isNextTurn);
                 }
+                j++;
             }
             
                 if(isMonsterDied(M)){
                     statusBattle = 1;
                 }
                 else{
+                    M.printMonster();
                     System.out.println("Giliran " + M.getName());
                     printStatus(P,M);
                     do{
@@ -129,10 +157,21 @@ public class Battle{
                 }
         }while(statusBattle == 0);
         if(statusBattle == 1){
+			System.out.println("Congratulation!\n");
         	M.giveMoney(P);
+        	System.out.println("Obtain " + M.getMoney() + " Gil\n");
         	for(int i = 0;i<=2;i++){
         		if(!isCharacterDied(P.getCharacters()[i])){
-        			P.getCharacters()[i].levelUP(M.getIncLevel());
+        			int j = 0;
+        			while ((j <= M.getIncLevel()) && (P.getCharacters()[i].getLV() < 40)) {
+        				j++;
+        				P.getCharacters()[i].levelUP(1);
+        			}
+        			if (j == 0) {
+        				System.out.println("Max level cap reached");
+        			} else {
+        				System.out.println(P.getCharacters()[i].getNama() + " increase " + j + " level");
+        			}
         		}
         		else{
         			P.getCharacters()[i].setCurHP(1);
