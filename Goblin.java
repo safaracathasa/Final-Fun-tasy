6527dc642d66a9ecc925f41f56ffe60d4ed0d957
Zonkies <mychar_id@yahoo.co.id>
2015-04-27 12:10:29 +0700

    Update Goblin.java

--- Goblin.java
+++ Goblin.java
@@ -1,15 +1,15 @@
-package dev.huntul.finalfuntasy.monster;
+package finalfuntasy.monster;
 
-import dev.huntul.finalfuntasy.character.CharacterGame;
+import finalfuntasy.character.CharacterGame;
 
 public class Goblin extends Monster {
 
     public Goblin (CharacterGame chars1,CharacterGame chars2,CharacterGame chars3)
     {
         super("Goblin",chars1,chars2,chars3);
-        this.ATK = (this.ATK*2)+5;
-        this.maxHP = (this.maxHP*40)+200;
-        this.CurHP = (this.CurHP*40)+200;
+        this.ATK = (this.ATK*5)+5;
+        this.maxHP = (this.maxHP*200)+200;
+        this.CurHP = (this.CurHP*200)+200;
         this.money = (this.money*3)+50;
         this.incLevel = 1;
     }
