91e0b7e9d198f8611dce0529d70f15a9f4026738
Zonkies <mychar_id@yahoo.co.id>
2015-04-27 12:12:45 +0700

    Update ItemFactory.java

--- ItemFactory.java
+++ ItemFactory.java
@@ -1,4 +1,4 @@
-package dev.huntul.finalfuntasy.item;
+package finalfuntasy.item;
 
 import java.util.Random;
 
@@ -8,11 +8,11 @@ public class ItemFactory {
 		Random rand = new Random();
 		int token = rand.nextInt(6);
 		if ((token == 0) || (token == 1) || (token == 2)) {
-			item = new HealingItem("Potion",50,50,1);
+			item = new HealingItem("Potion",200,50,1);
 		} else if ((token == 3) || (token == 4)) {
-			item = new HealingItem("Ether",25,100,2);
+			item = new HealingItem("Ether",100,100,2);
 		} else {
-			item = new AttackItem("Bomb",100,200);
+			item = new AttackItem("Bomb",300,200);
 		}
 		return item;
 	}
